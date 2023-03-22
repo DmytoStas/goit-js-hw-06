e704fae58cb7b69c67a7f700526311a384da9703
Dmytro Stas <dima7stas777@gmail.com>
2023-03-21 22:08:26 -0400

    task 1, 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientListRef = document.querySelector('#ingredients');
+
+function makeItem(item) {
+  const listItem = document.createElement('li');
+  listItem.textContent = item;
+
+  return listItem;
+}
+
+const listItemsArray = ingredients => {
+  return ingredients.map(makeItem);
+};
+
+ingredientListRef.append(...listItemsArray(ingredients));
